a4410a23c47e635fa9c7191c250f301519ab715c
briandunnington <briandunnington@gmail.com>
2025-02-14 15:33:28 -0800

    create nuget package
    also update namespace to match nuget package metadata

---
+++ Element.CloudDistributedLock/CloudDistributedLockProviderOptions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace Element.CloudDistributedLock
+{
+    public class CloudDistributedLockProviderOptions
+    {
+        internal string? ProviderName { get; set; }
+
+        internal CosmosClient? CosmosClient { get; set; }
+
+        public int TTL { get; set; } = 5;   // Should match the value set in Cosmos
+
+        public string? DatabaseName { get; set; }
+
+        public string ContainerName { get; set; } = "distributedlocks";
+
+        public TimeSpan RetryInterval { get; set; } = TimeSpan.FromMilliseconds(500);
+    }
+}
---
+++ Element.CloudDistributedLock/LockRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace Element.CloudDistributedLock
+{
+    public class LockRecord
+    {
+        public string? id { get; set; }
+        public string? name { get; set; }
+        public string? providerName { get; set; }
+        public DateTimeOffset lockObtainedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset lockLastRenewedAt { get; set; } = DateTimeOffset.UtcNow;
+        public int _ttl { get; set; }
+    }
+}
